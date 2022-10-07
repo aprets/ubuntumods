93a52c76180f9d6972730f9969a92eb85cafa4ee
Arthur <15954684+gentleseal@users.noreply.github.com>
2022-10-07 17:45:42 +0100

    feat: finish script

--- installMods.sh
+++ installMods.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 [ "$UID" -eq 0 ] || exec sudo "$0" "$@"
-echo "$(whoami)"
+echo "copying in edid firmware"
+mkdir -p /usr/lib/firmware/edid
+cp p278q.bin /usr/lib/firmware/edid/
+echo "adding kernel params"
+kernelstub -a "video=DP-1:e"
+kernelstub -a "drm.edid_firmware=DP-1:edid/p278q.bin"
+echo "copying in initramfs edid hook"
+cp initramfsEdidHook.sh /etc/initramfs-tools/hooks/edid
+echo "updating initramfs"
+update-initramfs -u -k all
+echo "appending mouse wake disable to rc.local"
+cat disableMouseWake.sh >> /etc/rc.local
+echo "installer done"
