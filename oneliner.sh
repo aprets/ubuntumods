49788dedd510bfaeb2cf83932ac9ed46bb1fd5e1
Arthur <15954684+gentleseal@users.noreply.github.com>
2022-10-07 17:30:03 +0100

    feat: add echos

--- oneliner.sh
+++ oneliner.sh
@@ -1,10 +1,15 @@
 cd /tmp
+echo "🪄 making temp memeods directory"
 mkdir memods
 cd memods
+echo "📥 downloading the stuff"
 curl -L https://github.com/aprets/ubuntumods/archive/refs/heads/main.zip -o main.zip
+echo "📦 unboxing the stuff"
 unzip main.zip -d main
 cd main/*/
+echo "💿 starting the installer"
 ./installMods.sh
 cd /tmp
+echo "💥 removing the temp dir"
 rm -rf memods
-echo "🤖"
+echo "🤖 donezo"
