b19943ad29808ad00dcfe650b79511254fb44625
Arthur <15954684+gentleseal@users.noreply.github.com>
2022-10-07 17:17:44 +0100

    feat: skeleton structure

---
+++ disableMouseWake.sh
@@ -0,0 +1,2 @@
+# Disable wake computer from mouse movement
+echo "disabled" > /sys/bus/usb/devices/3-6.4/power/wakeup
---
+++ installMods.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+[ "$UID" -eq 0 ] || exec sudo "$0" "$@"
+echo "$(whoami)"
+
+https://github.com/aprets/ubuntumods/raw/main/p278q.bin
