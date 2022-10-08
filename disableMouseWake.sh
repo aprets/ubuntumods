4c54437339983df94b2f32e10fd3407b1943c2cf
aprets <15954684+aprets@users.noreply.github.com>
2022-10-08 17:33:59 +0100

    fix: hopefully fix random wakes

--- disableMouseWake.sh
+++ disableMouseWake.sh
@@ -1,2 +1,4 @@
 # Disable wake computer from mouse movement
 echo "disabled" > /sys/bus/usb/devices/3-6.4/power/wakeup
+# Disable something sus
+echo "disabled" > /sys/bus/usb/devices/1-1/power/wakeup
