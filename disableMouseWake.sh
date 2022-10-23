f3ecb4e01fdfe7b9bf224c2871d778a8c20cb3d2
aprets <15954684+aprets@users.noreply.github.com>
2022-10-23 20:37:10 +0100

    fix: try hopefully working fix from instant wakes

--- disableMouseWake.sh
+++ disableMouseWake.sh
@@ -1,4 +1,4 @@
 # Disable wake computer from mouse movement
 echo "disabled" > /sys/bus/usb/devices/3-6.4/power/wakeup
-# Disable something sus
-echo "disabled" > /sys/bus/usb/devices/1-1/power/wakeup
+# Disabe the cringe
+echo "GPP0" > /proc/acpi/wakeup
